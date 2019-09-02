b0185a8f81dd8597a585b509fa1b539fca0375f2
S.H <gamelinks007@gmail.com>
2019-09-02 11:58:36 +0900

    Create if.go

---
+++ if.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+    sum := 1
+    
+    if sum >= 10 {
+        fmt.Println(sum)
+    }
+    
+    if sum > 10 {
+        fmt.Println(sum)   
+    }
+    
+    if sum <= 10 {
+        fmt.Println(sum)   
+    }
+    
+    if sum < 10 {
+        fmt.Println(sum)   
+    }
+    
+    if sum == 1 {
+        fmt.Println(sum)   
+    }
+    
+    if sum != 10 {
+        fmt.Println(sum)   
+    }
+}
